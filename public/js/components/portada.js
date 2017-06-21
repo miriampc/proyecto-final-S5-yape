3ca6b9bbb3ebc79e76e45e17269d0bef1931a171
miriampc <miiriampc@gmail.com>
2017-06-21 06:49:05 -0500

    carousel un construction

--- public/js/components/portada.js
+++ public/js/components/portada.js
@@ -1,15 +1,27 @@
 "use strict";
+const carousel = _=> {
+  const divCarousel=$('<div class="owl-carousel owl-theme"></div>');
+  const item1 = $(`<div class="item"><img src="img/icons/icon-people.png" alt="icon people">
+                <h1>Paga a tus amigos</h1>
+                <p>Paga a quien quieras desde donde quieras</p></div>`);
+  const item2 = $(`<div class="item"><img src="img/icons/happy-person.png" alt="happy person">
+                <h1>Sin número de cuenta</h1>
+                <p>Elige a quien pagar desde tu lista de contactos</p></div>`);
+  const item3 = $(`<div class="item"><img src="img/icons/group-people.png" alt=" group people">
+                <h1>Gratis y seguro</h1>
+                <p>La transferencia es inmediata y gratuita de una cuenta a otra</p></div>`);
+
+  divCarousel.append(item1);
+  divCarousel.append(item2);
+  divCarousel.append(item3);
+  return divCarousel;
+}
+
 const Portada = (update)=>{
     const containerPortada = $('<section class="container"></section>');
-    const slide = $(`<div class="slideImg"><img src="img/icons/icon-people.png" alt=""></div>`);
-    const title = $(`<h1>Paga a tus amigos</h1>`);
-    const description = $(`<p>paga a quien quieras desde donde quieras</p>`);
     const button = $('<button>Registrarme</button>');
 
-
-    slide.append(title);
-    slide.append(description);
-    containerPortada.append(slide);
+    containerPortada.append(carousel());
     containerPortada.append(button);
 
     button.on('click',_=>{
