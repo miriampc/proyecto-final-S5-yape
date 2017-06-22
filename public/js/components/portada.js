8abc4310cfd5654672e3004bc8e4aa913aae6379
Miriam Peralta <miiriampc@gmail.com>
2017-06-22 05:36:04 -0500

    styles css, card register in construction

--- public/js/components/portada.js
+++ public/js/components/portada.js
@@ -1,11 +1,12 @@
 "use strict";
 const Portada = (update)=>{
     const containerPortada = $('<section class="container"></section>');
-
+    const div = $('<div class="carousel flex"></div>');
     const button = $('<button>Registrarme</button>');
 
-    containerPortada.append(carousel());
-    containerPortada.append(button);
+    div.append(carousel());
+    div.append(button);
+    containerPortada.append(div);
 
     button.on('click',_=>{
         state.nextPage = RegisterNumber;
